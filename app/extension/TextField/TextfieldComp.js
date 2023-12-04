ab1d672fa949bdc6e6f65f3caaacfe7d6215ce25
tgsjenish <152592135+tgsjenish@users.noreply.github.com>
2023-12-04 16:57:18 +0530

    Update TextfieldComp.js

--- app/extension/TextField/TextfieldComp.js
+++ app/extension/TextField/TextfieldComp.js
@@ -3,18 +3,8 @@ import TextField from "@mui/material/TextField";
 import { html, useContext } from "diagram-js/lib/ui";
 
 export default function TextfieldComp() {
-  return <TextField id="outlined-basic" label="Outlined" variant="outlined" />;
+  return html`
+    <${TextField} id="outlined-basic" label="Outlined" variant="outlined" />
+  `;
 }
 
-// // return (
-// //   <>
-// //     <div>
-// //       <input type="text" />
-// //     </div>
-// //   </>
-// // );
-// {
-//   /* <div>
-//       <${TextField} id="outlined-basic" label="Outlined" variant="outlined" />
-//     </div> */
-// }
